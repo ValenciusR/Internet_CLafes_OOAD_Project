7e39ba68a51799f94e9584c4d063fa9a8913816a
Valencius Rianto <127145309+ValenciusR@users.noreply.github.com>
2023-12-07 13:00:27 +0700

    database

---
+++ internet_clafes.sql
@@ -0,0 +1,248 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 07, 2023 at 06:59 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.1.17
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `internet_clafes`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `job`
+--
+
+CREATE TABLE `job` (
+  `Job_ID` int(11) NOT NULL,
+  `UserID` int(11) NOT NULL,
+  `PC_ID` int(11) NOT NULL,
+  `JobStatus` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pc`
+--
+
+CREATE TABLE `pc` (
+  `PC_ID` int(11) NOT NULL,
+  `PC_Condition` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pcbook`
+--
+
+CREATE TABLE `pcbook` (
+  `BookID` int(11) NOT NULL,
+  `PC_ID` int(11) NOT NULL,
+  `UserID` int(11) NOT NULL,
+  `BookedDate` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `report`
+--
+
+CREATE TABLE `report` (
+  `Report_ID` int(11) NOT NULL,
+  `UserRole` varchar(255) NOT NULL,
+  `PC_ID` int(11) NOT NULL,
+  `ReportNote` varchar(255) NOT NULL,
+  `ReportDate` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transactiondetail`
+--
+
+CREATE TABLE `transactiondetail` (
+  `TransactionID` int(11) NOT NULL,
+  `PC_ID` int(11) NOT NULL,
+  `CustomerName` varchar(255) NOT NULL,
+  `BookTime` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transactionheader`
+--
+
+CREATE TABLE `transactionheader` (
+  `TransactionID` int(11) NOT NULL,
+  `StaffID` int(11) NOT NULL,
+  `StaffName` varchar(255) NOT NULL,
+  `TransactionDate` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `UserID` int(11) NOT NULL,
+  `UserName` varchar(255) NOT NULL,
+  `UserPassword` varchar(255) NOT NULL,
+  `UserAge` int(11) NOT NULL,
+  `UserRole` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `job`
+--
+ALTER TABLE `job`
+  ADD PRIMARY KEY (`Job_ID`),
+  ADD KEY `UserID` (`UserID`),
+  ADD KEY `PC_ID` (`PC_ID`);
+
+--
+-- Indexes for table `pc`
+--
+ALTER TABLE `pc`
+  ADD PRIMARY KEY (`PC_ID`);
+
+--
+-- Indexes for table `pcbook`
+--
+ALTER TABLE `pcbook`
+  ADD PRIMARY KEY (`BookID`),
+  ADD KEY `PC_ID` (`PC_ID`),
+  ADD KEY `UserID` (`UserID`);
+
+--
+-- Indexes for table `report`
+--
+ALTER TABLE `report`
+  ADD PRIMARY KEY (`Report_ID`),
+  ADD KEY `PC_ID` (`PC_ID`);
+
+--
+-- Indexes for table `transactiondetail`
+--
+ALTER TABLE `transactiondetail`
+  ADD KEY `PC_ID` (`PC_ID`),
+  ADD KEY `TransactionID` (`TransactionID`);
+
+--
+-- Indexes for table `transactionheader`
+--
+ALTER TABLE `transactionheader`
+  ADD PRIMARY KEY (`TransactionID`),
+  ADD KEY `transactionheader` (`StaffID`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`UserID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `job`
+--
+ALTER TABLE `job`
+  MODIFY `Job_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `pc`
+--
+ALTER TABLE `pc`
+  MODIFY `PC_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `pcbook`
+--
+ALTER TABLE `pcbook`
+  MODIFY `BookID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `report`
+--
+ALTER TABLE `report`
+  MODIFY `Report_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `transactionheader`
+--
+ALTER TABLE `transactionheader`
+  MODIFY `TransactionID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `job`
+--
+ALTER TABLE `job`
+  ADD CONSTRAINT `job_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `user` (`UserID`),
+  ADD CONSTRAINT `job_ibfk_2` FOREIGN KEY (`PC_ID`) REFERENCES `pc` (`PC_ID`);
+
+--
+-- Constraints for table `pcbook`
+--
+ALTER TABLE `pcbook`
+  ADD CONSTRAINT `pcbook_ibfk_1` FOREIGN KEY (`PC_ID`) REFERENCES `pc` (`PC_ID`),
+  ADD CONSTRAINT `pcbook_ibfk_2` FOREIGN KEY (`UserID`) REFERENCES `user` (`UserID`);
+
+--
+-- Constraints for table `report`
+--
+ALTER TABLE `report`
+  ADD CONSTRAINT `report_ibfk_1` FOREIGN KEY (`PC_ID`) REFERENCES `pc` (`PC_ID`);
+
+--
+-- Constraints for table `transactiondetail`
+--
+ALTER TABLE `transactiondetail`
+  ADD CONSTRAINT `transactiondetail_ibfk_1` FOREIGN KEY (`PC_ID`) REFERENCES `pc` (`PC_ID`),
+  ADD CONSTRAINT `transactiondetail_ibfk_2` FOREIGN KEY (`TransactionID`) REFERENCES `transactionheader` (`TransactionID`);
+
+--
+-- Constraints for table `transactionheader`
+--
+ALTER TABLE `transactionheader`
+  ADD CONSTRAINT `transactionheader` FOREIGN KEY (`StaffID`) REFERENCES `user` (`UserID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
